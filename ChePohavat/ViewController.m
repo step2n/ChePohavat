f2e1850f46076606df10995ba24e1d7d851eaa3f
Your Name <nikshebeda@gmail.com>
2015-09-24 22:24:59 +0300

    Initial Commit

---
+++ ChePohavat/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  ChePohavat
+//
+//  Created by Nik on 24.09.15.
+//  Copyright (c) 2015 Nik. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
