037d7f0af1e9c62b373b672544575d842a79068d
Your Name <nikshebeda@gmail.com>
2015-09-24 23:35:06 +0300

    new

--- ChePohavat/main.m
+++ ChePohavat/main.m
@@ -1,16 +1,18 @@
 //
 //  main.m
-//  ChePohavat
+//  SlideMenu
 //
-//  Created by Nik on 24.09.15.
-//  Copyright (c) 2015 Nik. All rights reserved.
+//  Created by Aryan Gh on 4/24/13.
+//  Copyright (c) 2013 Aryan Ghassemi. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
+
 #import "AppDelegate.h"
 
-int main(int argc, char * argv[]) {
-    @autoreleasepool {
-        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
-    }
+int main(int argc, char *argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
 }
